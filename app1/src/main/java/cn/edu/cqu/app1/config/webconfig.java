4a90a5c97c5fb2aea5529a715969b7f01dd74ac3
Rookiecyber <857582847@qq.com>
2021-11-05 16:43:38 +0800

    app1

---
+++ app1/src/main/java/cn/edu/cqu/app1/config/webconfig.java
@@ -0,0 +1,23 @@
+package cn.edu.cqu.app1.config;
+
+import cn.edu.cqu.app1.interceptor.Interceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webconfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(appInterceptor())
+                .addPathPatterns("/**") // 拦截所有请求，
+                .excludePathPatterns("app1/index")
+                .excludePathPatterns("/static/**");
+    }
+
+    @Bean
+    public Interceptor appInterceptor(){
+        return  new Interceptor();
+    }
+}
---
+++ app1/src/main/java/cn/edu/cqu/app1/interceptor/Interceptor.java
@@ -0,0 +1,51 @@
+package cn.edu.cqu.app1.interceptor;
+
+import cn.edu.cqu.authserver.util.JwtUtil;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.alibaba.fastjson.JSONObject;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Interceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        /*读取ck*/
+        String token = "";
+        Cookie[] cookies = request.getCookies();
+        Cookie cookie = null;
+        for (int j = 0; j < cookies.length; j++) {
+            cookie = cookies[j];
+            if (cookie.getName().equals("token")) {
+                token = cookie.getValue();
+            }
+        }
+        System.out.println("token是"+token);
+        if(token != null){
+            if(JwtUtil.verify(token)){
+                return true;
+            }
+        }
+        response.sendRedirect("http://127.0.0.1:8080/auth/login"); //重定向到登录页面
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");
+        System.out.println(request.getServletPath());
+        try{
+            JSONObject json = new JSONObject();
+            json.put("success","false");
+            json.put("msg","认证失败，未通过拦截器");
+            json.put("code","50000");
+            response.getWriter().append(json.toJSONString());
+            System.out.println("认证失败，未通过拦截器");
+        }catch (Exception e){
+            e.printStackTrace();
+            response.sendError(500);
+            return false;
+        }
+        return false;
+    }
+}
